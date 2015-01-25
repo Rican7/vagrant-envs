f401a8e605c6a4f3e4e0e364d2ad3c138b47c565
Trevor Suarez <rican7@gmail.com>
2015-01-25 17:48:20 -0500

    Now installing Ruby 1.9.3 with RHEL Software Collections

--- provisioning/shell.sh
+++ provisioning/shell.sh
@@ -7,8 +7,24 @@
 # Update core packages via Yum
 yum -y update
 
-# Install `yum-utils`
-yum -y install yum-utils
+# Install `yum-utils` and `scl-utils`
+yum -y install yum-utils scl-utils
+
+
+
+##################
+## Ruby Install ##
+##################
+
+# Install the Ruby software-collections repo package
+# Download and enable the Ruby software-collections repo
+rpm -ivh https://www.softwarecollections.org/en/scls/rhscl/ruby193/epel-6-x86_64/download/rhscl-ruby193-epel-6-x86_64.noarch.rpm
+
+# Install ruby via Yum
+yum -y install ruby193-ruby
+
+# Run a new bash instance with the new ruby version
+scl enable ruby193 bash
 
 
 
