46258f5fe16cc8f01e116f7a77f04f8bcf76875d
Trevor Suarez <rican7@gmail.com>
2015-02-03 21:30:23 -0500

    Finally fed up with a non-standard Ruby install. Puppet plugins hate it, so I'm out.
    Anyway, Puppet is very easily compatible by simply switching to using
    `librarian-puppet` v1.x

--- provisioning/shell.sh
+++ provisioning/shell.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-###############
-## Variables ##
-###############
+#############################
+## Additional Repo Install ##
+#############################
 
-# Path to our ruby profile script
-RUBY_PROFILE_SCRIPT_PATH="/etc/profile.d/ruby193.sh"
+# Download and enable the EPEL repo
+rpm -ivh https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+
+# Download the Remi repo (http://rpms.famillecollet.com/)
+rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+
+# Enable the Remi base "remi" repo to be queried by default
+yum-config-manager --enable remi
 
 
 
@@ -23,27 +29,11 @@ yum -y install \
     git \
     gcc \
     kernel-devel \
-    openssl-devel
+    openssl-devel \
+    ruby \
+    ruby-devel \
+    rubygems
 
-
-
-##################
-## Ruby Install ##
-##################
-
-# Install the Ruby software-collections repo package
-# Download and enable the Ruby software-collections repo
-rpm -ivh https://www.softwarecollections.org/en/scls/rhscl/ruby193/epel-6-x86_64/download/rhscl-ruby193-epel-6-x86_64.noarch.rpm
-
-# Install ruby via Yum
-yum -y install ruby193-ruby ruby193-ruby-devel
-
-# Set the new ruby version as the default for all users
-echo "source /opt/rh/ruby193/enable" > $RUBY_PROFILE_SCRIPT_PATH
-echo 'export PATH="/opt/rh/ruby193/root/usr/local/bin:$PATH"' >> $RUBY_PROFILE_SCRIPT_PATH
-
-# Source our new profile
-source $RUBY_PROFILE_SCRIPT_PATH
 
 
 
@@ -59,18 +49,3 @@ gem install bundler
 
 # Install our puppet modules
 (cd /vagrant/provisioning/puppet && exec librarian-puppet install)
-
-
-
-#############################
-## Additional Repo Install ##
-#############################
-
-# Download and enable the EPEL repo
-rpm -ivh https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-
-# Download the Remi repo (http://rpms.famillecollet.com/)
-rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-
-# Enable the Remi base "remi" repo to be queried by default
-yum-config-manager --enable remi
